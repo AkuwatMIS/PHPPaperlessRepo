dd844844157f30c285a1577a7a06d473c8bfdc14
H M Asif Javed <asif.ghulamrasool@akhuwat.org.pk>
2025-07-11 03:01:43 -0700

    commit

--- console/controllers/NotificationsController.php
+++ console/controllers/NotificationsController.php
@@ -124,9 +124,10 @@ class NotificationsController extends Controller
 
                 $sms_log = new SmsLogs();
 
-                $sms_log->user_id = $q['id'];
+                $sms_log->user_id = 1;
                 $sms_log->number = $mobile;
-                $sms_log->sms_type = 'CNIC Expiry Alert!';
+                $sms_log->sms_type = 'member';
+                $sms_log->type_id = $q['id'];
 
                 if (isset($sms->corpsms[0]->type) && $sms->corpsms[0]->type === 'Success') {
                     $sms_log->status = 1;
