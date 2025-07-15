d6e44f908dd8255e87f48c872819dd768f56e55f
H M Asif Javed <asif.ghulamrasool@akhuwat.org.pk>
2025-07-15 03:57:40 -0700

    commit

--- console/controllers/HousingDashboardController.php
+++ console/controllers/HousingDashboardController.php
@@ -587,7 +587,8 @@ class HousingDashboardController extends Controller
                 $i++;
             }
 
-            print_r($loans_data);
+            print_r(json_encode($loans_data));
+            die();
 
             $headers = array
             (
@@ -599,17 +600,15 @@ class HousingDashboardController extends Controller
 
 
             $ch = curl_init('http://20.174.13.174/post_data_project_details.php');
-            echo $ch;
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($loans_data));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-           
+
             $result = curl_exec($ch);
             print_r($result);
             curl_close($ch);
-            die();
         }
 
     }
