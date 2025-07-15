6d8652ee7adcdde42d3c80a25d555dc13380c9f4
H M Asif Javed <asif.ghulamrasool@akhuwat.org.pk>
2025-07-15 00:35:42 -0700

    commit

--- console/controllers/NotificationsController.php
+++ console/controllers/NotificationsController.php
@@ -88,7 +88,7 @@ class NotificationsController extends Controller
     {
         $date = date("Y-m-11");
         $date2 = date("Y-m-13", strtotime("+1 month"));
-        $nextDate = date('',$date2);
+        $nextDate = date('Y-m-d',$date2);
 
 
         echo $date;
