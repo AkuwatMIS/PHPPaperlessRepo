c42bcb6e9fd3cd816ffb3c46de923c1bc74cbcd0
H M Asif Javed <asif.ghulamrasool@akhuwat.org.pk>
2025-07-26 01:35:23 -0700

    commit

--- console/controllers/GeneralInfoController.php
+++ console/controllers/GeneralInfoController.php
@@ -8724,8 +8724,10 @@ where 1 and l.status in ('collected','loan completed') and l.deleted=0 and l.act
                 "FirstDueDate"=>null,
                 "SecondDisbursementDate"=> null,
             ];
+            print_r($obj);
+            die();
             AcagHelper::actionPushDisbursement($obj);
-            echo 'success-'.$cnic_without_hyphens.'---';
+            echo '---'.$cnic_without_hyphens.'---';
         }
     }
 
